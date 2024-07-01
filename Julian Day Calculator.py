ec1a3228ac6b8e3ff30707317595bbdf40e4bf9c
Mihir Bhagat <mihirbhagat15@gmail.com>
2024-07-01 15:09:54 -0400

    fixed J0 calculation for Julian Day Calculator added errorvalue raise, corrected Stumpff Functions, added root check, improved readability for Lamberts

--- Julian Day Calculator.py
+++ Julian Day Calculator.py
@@ -1,28 +1,30 @@
-# %%
 import datetime
 
-print("Input the year:")
-Y = float(input())
-print("Input the month:")
-M = float(input())
-print("Input the day:")
-D = float(input())
-print("Input the universal time:")
-time = str(input())
+# Input year, month, and day as ints
+Y = int(input("Input the year: "))
+M = int(input("Input the month: "))
+D = int(input("Input the day: "))
 
+# Input universal time
+time = input("Input the universal time (HH:MM:SS): ")
+
+# Parse the time string
 time_obj = datetime.datetime.strptime(time, "%H:%M:%S")
 
-hour = time_obj.hour
-minute = time_obj.minute
-seconds = time_obj.second
-# %%
-UT = hour + (minute/60) + (seconds/3600)
+# Extract hours, minutes, and seconds
+hours, minutes, seconds = time_obj.hour, time_obj.minute, time_obj.second
 
-J0 = (367*Y) - int((7*(Y+int((M+9)/12)))/4) + int((275*M)/9)
-+ D + 17121013.5
+# Calculate UT in decimal hours
+UT = hours + (minutes/60) + (seconds/3600)
 
-print(UT)
-print(J0)
-JD = J0 + (UT / 24)
+# Calculate Julian Date
+if M <= 2:
+    Y -= 1
+    M += 12
 
-print(f"The Julian Day Number is: {JD}")
+A = int(Y / 100)
+B = 2 - A + int(A / 4)
+
+JD = int(365.25 * (Y + 4716)) + int(30.6001 * (M + 1)) + D + B - 1524.5 + (UT / 24)
+
+print(f"The Julian Date is: {JD}")
